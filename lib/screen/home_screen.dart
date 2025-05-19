35da8cb9e4e54d639afd0b68d4ed6a4ea3a8400b
sethh99 <unpiseth07@gmail.com>
2025-05-19 20:12:55 +0700

    change name

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -9,7 +9,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Home Screen'),
         centerTitle: true,
       ),
       body: Center(
